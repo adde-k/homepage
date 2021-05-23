16c753d1966183ad10e78cf1ded62d7d5d28b448
Adiba Khan <nadibakhan@gmail.com>
2021-05-23 20:40:11 +0200

    Add contanct_us_v1

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
+
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
