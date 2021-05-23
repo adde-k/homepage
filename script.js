3328098c1750d5ee0eb852ec4bcf15edebbe5d4b
Adiba Khan <nadibakhan@gmail.com>
2021-05-23 20:47:23 +0200

    add contact_us_v1 again

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function currentSlide(n) {
 //
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlideLinks");
+  var slides = document.getElementsByClassName("mySlideLink");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex =1}
   if (n < 1) {slideIndex = slides.length}
